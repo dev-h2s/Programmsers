b3374f07583c81ae63ffb55318dfa86049f0c2c2
dev-h2s <2esc2esch@gmail.com>
2024-08-20 01:00:17 +0900

    [level 0] Title: 두 수의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.py
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.py
@@ -1,3 +1,7 @@
 def solution(num1, num2):
-    answer = num1+num2
+    answer = -1
+    max = 50000
+    min = -50000
+    if (num1>=-50000 or num1<=50000) and (num2>=-50000 or num2<=50000) :
+        answer = num1+num2
     return answer
