b926d07be391ddbc1e3932cf9deb899d0632b458
dev-h2s <2esc2esch@gmail.com>
2024-08-19 11:55:06 +0900

    [level 0] Title: 짝수의 합, Time: 0.15 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=0; i<n+1; i+=2){
+            answer+=i;
+        }
+        return answer;
+    }
+}
